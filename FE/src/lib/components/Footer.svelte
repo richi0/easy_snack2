527c0e66bccc6c10f62dc37526c6ed5f89d87f9e
Richi0 <csspliskin@hotmail.com>
2022-07-03 14:11:36 +0200

    ignored caddy files

---
+++ FE/src/lib/components/Footer.svelte
@@ -0,0 +1,10 @@
+<footer>
+  <a href="/subscribe">Subscribe</a>
+  <a href="/disclaimer">Disclaimer</a>
+</footer>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
---
+++ FE/src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<nav>
+  <a href="/">Blog</a>
+  <a href="/map">Map</a>
+  <a href="/country">Countrie</a>
+  <a href="/city">Cities</a>
+  <a href="/about">About</a>
+</nav>
